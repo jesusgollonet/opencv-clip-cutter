bb7c51495a5b9c3da87ed55be34582a00b3091af
Jesus Gollonet <jesusgollonet@gmail.com>
2023-07-03 10:22:57 +0200

    initial setup

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import sys
 import cv2 as cv
 
+cap = cv.VideoCapture(0)
 
+cv.namedWindow("Video")
 
+if not cap.isOpened():
+    print("error opening video")
+    sys.exit(1)
+
+while (cap.isOpened()):
+     ret, frame = cap.read()
+     if ret is True:
+         cv.imshow("Video", frame)
+
+         if cv.waitKey(25) & 0xFF == ord('q'):
+             break
+     else:    
+         break
+
+cap.release()
+cv.destroyAllWindows()
