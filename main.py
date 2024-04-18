264d9b1243ba800bfb1604d9b5d6ebd35eeb5d84
Jesus Gollonet <jesusgollonet@gmail.com>
2024-04-18 17:13:52 +0200

    Apply bg subtraction

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ cap = cv.VideoCapture(video_path)
 
 frames = []
 bw_frames = []
+bs_frames = []
 c = 0
 
 frame_w = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
@@ -16,6 +17,9 @@ frame_h = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 # total number of frames in the video
 total_frames = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
 cols = 20
+
+
+bs = cv.createBackgroundSubtractorKNN()
 
 # store frames
 for i in range(total_frames):
@@ -27,9 +31,11 @@ for i in range(total_frames):
     # threshold image
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     _, bw_frame = cv.threshold(gray_frame, 127, 255, cv.THRESH_BINARY_INV)
+    fg_mask = bs.apply(frame)
 
     frames.append(frame)
     bw_frames.append(bw_frame)
+    bs_frames.append(fg_mask)
 
 
 # given the total number of frames and the number of columns
@@ -40,14 +46,17 @@ mosaic_rows = (len(frames) // cols) + 1
 # create a blank mosaic with the correct dimensions
 mosaic = np.zeros((mosaic_rows * frame_h, cols * frame_w, 3), dtype=np.uint8)
 bw_mosaic = np.zeros((mosaic_rows * frame_h, cols * frame_w), dtype=np.uint8)
+bs_mosaic = np.zeros((mosaic_rows * frame_h, cols * frame_w), dtype=np.uint8)
 
 for i, frame in enumerate(frames):
     x = int((i % cols) * frame_w)
     y = int((i // cols) * frame_h)
     mosaic[y : y + frame_h, x : x + frame_w] = frame
     bw_mosaic[y : y + frame_h, x : x + frame_w] = bw_frames[i]
+    bs_mosaic[y : y + frame_h, x : x + frame_w] = bs_frames[i]
 
 cv.imwrite("output/mosaic.jpg", mosaic)
 cv.imwrite("output/mosaic_bw.jpg", bw_mosaic)
+cv.imwrite("output/mosaic_bs.jpg", bs_mosaic)
 cv.imshow("Mosaic", bw_mosaic)
 cv.waitKey(0)
