4761bd82264c480e87bc556613556f15db16aaf7
Jesus Gollonet <jesusgollonet@gmail.com>
2024-04-18 16:37:46 +0200

    Don't resize frames (start with a smaller source video)

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import cv2 as cv
 import numpy as np
 
@@ -10,40 +9,37 @@ cap = cv.VideoCapture(video_path)
 frames = []
 c = 0
 
-frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+frame_w = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+frame_h = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
 # total number of frames in the video
 total_frames = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
 cols = 20
-print("Total frames:", total_frames)
 
-resized_w = int(frame_width / 2)
-resized_h = int(frame_height / 2)
-
-print("Resized frame width:", resized_w, "Resized frame height:", resized_h)
-
+# store frames
 for i in range(total_frames):
+    # set the frame position
     cap.set(cv.CAP_PROP_POS_FRAMES, i)
     ret, frame = cap.read()
     if not ret:
         break
 
-    # resized_frame = cv.resize(frame, (resized_w, resized_h))
-    frames.append(resized_frame)
+    frames.append(frame)
 
 
-print(len(frames))
+# given the total number of frames and the number of columns
+# calculate the number of rows for the mosaic
+# note the +1 to account for the last row
 mosaic_rows = (len(frames) // cols) + 1
-print(mosaic_rows, cols)
 
-mosaic = np.zeros((mosaic_rows * resized_h, cols * resized_w, 3), dtype=np.uint8)
-print(mosaic.shape)
+# create a blank mosaic with the correct dimensions
+mosaic = np.zeros((mosaic_rows * frame_h, cols * frame_w, 3), dtype=np.uint8)
 
 for i, frame in enumerate(frames):
-    x = int((i % cols) * resized_w)
-    y = int((i // cols) * resized_h)
-    mosaic[y : y + resized_h, x : x + resized_w] = frame
+    x = int((i % cols) * frame_w)
+    y = int((i // cols) * frame_h)
+    mosaic[y : y + frame_h, x : x + frame_w] = frame
 
 cv.imwrite("output/mosaic.jpg", mosaic)
 cv.imshow("Mosaic", mosaic)
+cv.waitKey(0)
