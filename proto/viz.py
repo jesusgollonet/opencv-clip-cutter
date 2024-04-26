cf21887e554205a930ab1f82519baf8ba5bad2c9
Jesus Gollonet <jesusgollonet@gmail.com>
2024-04-26 12:41:29 +0200

    show text

--- proto/viz.py
+++ proto/viz.py
@@ -4,15 +4,26 @@ img = cv.imread("output/mosaic.jpg")
 cap = cv.VideoCapture("video/2024-01-13 08.38.31.mov")
 
 bgSub = cv.createBackgroundSubtractorMOG2()
+frame_shape = (640, 360)
+w = frame_shape[0]
+h = frame_shape[1]
+
+
+def text(frame, text, coords):
+    cv.putText(frame, text, coords, cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    resized = cv.resize(frame, (640, 360), interpolation=cv.INTER_AREA)
+    resized = cv.resize(frame, frame_shape)
     sub = bgSub.apply(resized)
-    cv.imshow("Video", resized)
-    cv.imshow("Sub", sub)
+    sub = cv.cvtColor(sub, cv.COLOR_GRAY2BGR)
+    combined = cv.hconcat([resized, sub])
+    text(combined, "Original", (10, 20))
+    text(combined, "Bg", (w + 10, 20))
+    cv.imshow("Video", combined)
     if cv.waitKey(1) == ord("q"):
         break
