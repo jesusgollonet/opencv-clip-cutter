96bcaf81fc8e01c7468592206ec473100eeb0fa9
Jesus Gollonet <jesusgollonet@gmail.com>
2024-04-29 15:35:37 +0200

    w, h => frame_w, _h

--- proto/viz.py
+++ proto/viz.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 
+# initialize vars
 videos = [
     "video/2024-01-13 08.38.31.mov",
     "video/2024-01-13 08.41.02.mov",
@@ -11,23 +12,20 @@ videos = [
     "video/test.mov",
 ]
 
-# use random video
 cap = cv.VideoCapture(videos[6])
-
-bgSub = cv.createBackgroundSubtractorMOG2()
 frame_shape = (640, 360)
-w = frame_shape[0]
-h = frame_shape[1]
-
+frame_w = frame_shape[0]
+frame_h = frame_shape[1]
 total_frames = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+bgSub = cv.createBackgroundSubtractorMOG2()
 
 
 def text(frame, text, coords):
     cv.putText(frame, text, coords, cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
 
-rect_img = np.zeros((100, w * 2, 3), np.uint8)
-rect = cv.rectangle(rect_img, (0, 0), (w * 2, 100), (0, 0, 0), -1)
+graph_img = np.zeros((100, frame_w * 2, 3), np.uint8)
+rect = cv.rectangle(graph_img, (0, 0), (frame_w * 2, 100), (0, 0, 0), -1)
 c = 0
 
 white_px_pct_ar = []
@@ -40,8 +38,10 @@ while True:
     resized = cv.resize(frame, frame_shape)
     sub = bgSub.apply(resized)
     white_px = cv.countNonZero(sub)
-    white_px_pct = white_px / (w * h) * 100
+    white_px_pct = white_px / (frame_w * frame_h) * 100
+    print(white_px_pct)
 
+    # make lower values more visible + truncate to 100
     white_px_pct_scaled = min(int(white_px_pct * 10), 100)
     white_px_pct_ar.append(white_px_pct_scaled)
 
@@ -52,9 +52,9 @@ while True:
     # from here it's only about displaying
     sub = cv.cvtColor(sub, cv.COLOR_GRAY2BGR)
     graph_pct = c / total_frames
-    rect_x = int(graph_pct * w * 2)
+    rect_x = int(graph_pct * frame_w * 2)
     frame_pct = cv.rectangle(
-        rect_img,
+        graph_img,
         (rect_x, 100),
         (rect_x, 100 - int(white_px_pct_scaled)),
         (255, 255, 255),
@@ -66,28 +66,28 @@ while True:
     combined = cv.vconcat([combined, rect])
 
     text(combined, "Original", (10, 20))
-    text(combined, "Bg", (w + 10, 20))
-    text(combined, f"White px %: {white_px_pct:.2f}%", (w + 10, 40))
-    text(combined, f"White px over time %: {white_px_pct:.2f}%", (10, h + 10))
-    text(combined, f"mean %: {mean:.2f}%", (10, h + 30))
+    text(combined, "Bg", (frame_w + 10, 20))
+    text(combined, f"White px %: {white_px_pct:.2f}%", (frame_w + 10, 40))
+    text(combined, f"White px over time %: {white_px_pct:.2f}%", (10, frame_h + 10))
+    text(combined, f"mean %: {mean:.2f}%", (10, frame_h + 30))
     cv.line(
         combined,
-        (0, (h + 100) - int(mean)),
-        (w * 2, (h + 100) - int(mean)),
+        (0, (frame_h + 100) - int(mean)),
+        (frame_w * 2, (frame_h + 100) - int(mean)),
         (255, 255, 255),
         2,
     )
     cv.line(
         combined,
-        (0, (h + 100) - int(median)),
-        (w * 2, (h + 100) - int(median)),
+        (0, (frame_h + 100) - int(median)),
+        (frame_w * 2, (frame_h + 100) - int(median)),
         (0, 255, 255),
         2,
     )
     cv.line(
         combined,
-        (0, (h + 100) - int(std)),
-        (w * 2, (h + 100) - int(std)),
+        (0, (frame_h + 100) - int(std)),
+        (frame_w * 2, (frame_h + 100) - int(std)),
         (255, 0, 0),
         2,
     )
